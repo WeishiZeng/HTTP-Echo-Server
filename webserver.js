256b09f0f06394a772ff6fc3cb012dba36ae8fd0
Weishi Zeng <weishi.zeng@oracle.com>
2017-02-05 01:31:59 -0800

    adding pug.js/jade.js

--- webserver.js
+++ webserver.js
@@ -1,9 +1,13 @@
 var http = require("http");
-var fs = require('fs');
 url = require('url');
 var port = 8080;  //http port
 var serverUrl = "0.0.0.0";
 var counter = 0; 
+
+//pug
+const pug = require('pug');
+const compiledFunction = pug.compileFile('template.pug');
+
 
 var server = http.createServer(function(req, res) {
 
@@ -11,6 +15,8 @@ var server = http.createServer(function(req, res) {
   console.log("Request: " + req.url + " (" + counter + ")"); //most browser gonna request for /favicon.ico
   
   if(req.url == "/") {
+  
+  var temp = {"headers": {"header1":"val1", "head2":"val2"}, "body":"test body"};
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
 	console.log("Request: " + req.method);
@@ -18,26 +24,14 @@ var server = http.createServer(function(req, res) {
 	console.log("Request: " + req.url);
 	var text = 'Hello World\n';
 	
+	text = 	compiledFunction(temp);
 	res.end(text);
 	return;
 
-  } 
-
-//   var patt = new RegExp(".jpg");
-//   if (patt.test(req.url)) {
-//     var action = req.pathname;
-//     console.log("Image " + action + ":" + req.url);
-// 
-//      var img = fs.readFileSync('./'+req.url);
-//      res.writeHead(200, {'Content-Type': 'image/gif' });
-//      res.end(img, 'binary');
-// 
-//      return;
-//   
-//   }
+  }
 
   res.setHeader("Content-Type", "text/html");
-  res.end("<p>Yo Anna! You are at " + req.url + ". Request counter: " + counter + "</p>");
+  res.end("<p>You are at " + req.url + ". Request counter: " + counter + "</p>");
 
 });
 
