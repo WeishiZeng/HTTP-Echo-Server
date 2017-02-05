2e1bf055222c8f96b3a9b74547257b6ac6b9c281
Weishi Zeng <weishi.zeng@oracle.com>
2017-02-05 02:20:15 -0800

    late night

--- webserver.js
+++ webserver.js
@@ -7,28 +7,50 @@ var counter = 0;
 //pug
 const pug = require('pug');
 const compiledFunction = pug.compileFile('template.pug');
-
+var headers;
+var body;
+var url;
 
 var server = http.createServer(function(req, res) {
 
-  counter++;   
-  console.log("Request: " + req.url + " (" + counter + ")"); //most browser gonna request for /favicon.ico
-  
+
+//TODO ignore /favicon.ico
+
+  //main page, provide details about previous request
   if(req.url == "/") {
-  
   var temp = {"headers": {"header1":"val1", "head2":"val2"}, "body":"test body"};
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
 	console.log("Request: " + req.method);
 	console.log("Request: " + JSON.stringify(req.headers));
 	console.log("Request: " + req.url);
-	var text = 'Hello World\n';
 	
-	text = 	compiledFunction(temp);
-	res.end(text);
+	//null check previous request
+	if (!headers) {
+		res.setHeader("Content-Type", "text/html");
+		
+		//TODO not writing html in js?
+		var html = "<h2 align=\"center\">HTTP Echo Server</h2> <p>No request captured yet.</p>";
+		res.end(html);
+		return;
+	}
+	//prepare data
+	//TODO think of separate names from pug file
+	var data = {"headers" : headers, "body" : body, "url": url};
+
+	res.end(compiledFunction(data));
 	return;
 
   }
+  
+	counter++;   
+  console.log("Request: " + req.url + " (" + counter + ")"); //most browser gonna request for /favicon.ico
+  
+  
+  //api calls, capture this request
+  headers = req.headers;
+  body = req.body;
+  url = req.url;
 
   res.setHeader("Content-Type", "text/html");
   res.end("<p>You are at " + req.url + ". Request counter: " + counter + "</p>");
